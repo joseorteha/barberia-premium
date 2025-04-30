eb54256a3749873653ae5196ebbf8553c6640d5b
joseorteha <joseortegahac@gmail.com>
2025-04-30 12:24:46 -0600

    Primer commit del proyecto Barbería Premium

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss';
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D4AF37', // Color dorado personalizado
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
